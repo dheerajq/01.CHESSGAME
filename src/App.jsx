e8807fc127eb4d251e3d2ffe9f523b281e00d748
dheerajq <39629207+dheerajq@users.noreply.github.com>
2024-05-21 17:06:34 +0530

    basic chess structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ChessBoard from "./components/ChessBoard";
+import "./App.css";
+function App() {
+  return (
+    <div className="app">
+      <ChessBoard />
+    </div>
+  );
+}
+
+export default App;
